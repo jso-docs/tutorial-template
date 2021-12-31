0f76a6af99a5d296ae4dce932a59065d366a52fb
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-12-31 12:16:06 +0100

    Trying to fix figure sources

--- getdeps.sh
+++ getdeps.sh
@@ -2,5 +2,5 @@
 # Script to clone the jso-docs main repo and get the necessary folders
 wget https://github.com/jso-docs/jso-docs.github.io/archive/refs/heads/main.zip
 unzip main.zip
-mv -f jso-docs.github.io-main/{_assets,_layout,_sass,_libs,config.md,package.json,utils.jl} .
+mv -f jso-docs.github.io-main/{_assets,_layout,_sass,_libs,package.json,utils.jl} .
 rm -rf jso-docs.github.io-main main.zip
