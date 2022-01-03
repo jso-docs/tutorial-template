5b71b362375262a5be07c0fca9df0bbe8c692d30
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-01-03 22:41:31 +0100

    Hide the plot command

--- index.jl
+++ index.jl
@@ -21,6 +21,7 @@ norm(A * x - b), norm(x .- 1)
 ## Plotting
 
 Use `joinpath("__site/assets", filename)` as the path to save the image in the right place and use `{{ rfig filename.ext Caption of image }}` to add the image afterwards.
+It is a good idea to use `# hide` so the save command doesn't appear. See the example below.
 =#
 
 using Plots
@@ -28,7 +29,6 @@ using Plots
 x = rand(3)
 y = rand(3)
 plot(x, y)
-png(joinpath("__site/assets", "myplot"))
-pwd()
+png(joinpath("__site/assets", "myplot")) # hide
 
 # {{ rfig myplot.png Example of plot and description }}
