5410554d43c8935e1211d67483d84b79dfc8ff34
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-01-14 19:30:31 +0100

    Update getbash.sh

--- getdeps.sh
+++ getdeps.sh
@@ -3,6 +3,7 @@
 wget https://github.com/jso-docs/jso-docs.github.io/archive/refs/heads/main.zip
 unzip main.zip
 if [ -z "$CI" ]; then
+  rm -rf _assets _layout _libs _sass
   mv -f jso-docs.github.io-main/{_assets,_layout,_sass,_libs,package.json,config.md,utils.jl} .
 else
   mv -f jso-docs.github.io-main/{_layout,_libs,_sass,package.json,config.md,utils.jl} .
