2264a30bbfbff2b76bb128a8475a05ca43f5ff9e
abelsiqueira <nepper271@gmail.com>
2022-03-08 21:45:31 +0000

    Deploying to gh-pages from  @ a482144ab6c305dca5a50b6eddacebb736417473 🚀

---
+++ update-blogposts.jl
@@ -0,0 +1,61 @@
+using Dates, Emporium, GitHub
+
+files_to_keep_updated = [
+  ".github/workflows/CompatHelper.yml",
+  ".github/workflows/Deploy.yml",
+  ".editorconfig",
+  ".gitignore",
+  "getdeps.sh",
+]
+
+clone_dir = "cloned-repos"
+
+# Clone all jso-docs repos
+auth = GitHub.authenticate(ENV["GITHUB_AUTH"])
+user = GitHub.whoami(auth = auth).login
+Emporium.clone_organization_repos(
+  "jso-docs",
+  clone_dir,
+  auth = auth,
+  dry_run = false,
+  exclude = ["jso-docs.github.io", "tutorial-template"],
+)
+
+# Copy all files_to_keep_updated
+path = pwd()
+for file in files_to_keep_updated
+  Emporium.run_on_folders(
+    (; kwargs...) -> begin
+      cp(joinpath(path, file), file, force=true)
+      run(`git add $file`)
+    end,
+    clone_dir,
+  )
+end
+
+# Branch and commit
+branch = Dates.format(now(), "Y-mm-dd-") * bytes2hex(rand(UInt8, 4))
+Emporium.run_on_folders(
+  (; basename = "", dirname = "", kwargs...) -> begin
+    if Emporium.git_has_to_commit()
+      run(`git checkout -b $branch`)
+      run(`git commit -m ":wrench: Updating files to keep up with template"`)
+      run(`git push -u origin $branch`)
+      Emporium.create_pull_request(
+        "jso-docs/$basename",
+        "Updating files to keep up with template",
+        "This semi-automated pull request is updating files to keep up with the jso-docs/tutorial-template.\n\ncc @$user",
+        branch,
+        auth = auth,
+        dry_run = false,
+        base = "main",
+      )
+    else
+      println("Nothing to do for $basename")
+    end
+  end,
+  clone_dir,
+)
+
+# Clean up
+run(`rm -rf $clone_dir`)
